983116f0cab1781ddcfa73498fa4979f7964ea9a
照井寛也 <s.3.h.st.sp211@gmail.com>
2019-12-09 23:31:32 +0900

    create handler

--- server.go
+++ server.go
@@ -1,5 +1,37 @@
 package main
 
-main(){
+import (
+	"net/http"
+)
 
+type User struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func main() {
+	server := http.Server{
+		Addr: ":8080",
+	}
+	http.HandleFunc("/post", handleRequest)
+	server.ListenAndServe()
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	var err error
+	switch r.Method {
+	case "GET":
+		err = handleGet(w, r)
+	case "POST":
+		err = handlePost(w, r)
+	case "PUT":
+		err = handlePut(w, r)
+	case "DELETE":
+		err = handleDelete(w, r)
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
