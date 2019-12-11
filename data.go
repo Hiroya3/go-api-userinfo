6d98a66b7b945dfbbce960013b18b06e95e37cad
照井寛也 <s.3.h.st.sp211@gmail.com>
2019-12-11 09:15:01 +0900

    add comment of sql.Open

--- data.go
+++ data.go
@@ -1,16 +1,19 @@
 package main
 
-import(
+import (
 	"database/sql"
-	_ "github.com/lib/qp"
+
+	_ "github.com/lib/pq"
 )
 
+//データベースへのハンドル（構造体）
 var Db *sql.Db
 
-func init(){
+func init() {
 	var err error
-	DB, err := sql.Open("postgres","user=dbuser dbname=user_info password=pass sslmode=disable")
-	if err != nil{
+	//Openを使っているが、これはsql.Dbに接続に必要な構造体を設定するのみ
+	DB, err := sql.Open("postgres", "user=dbuser dbname=user_info password=pass sslmode=disable")
+	if err != nil {
 		panic(err)
 	}
 }
